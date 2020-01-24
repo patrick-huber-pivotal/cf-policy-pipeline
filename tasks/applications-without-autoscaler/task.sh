296634a82e6d2b7dc2d853af08809eea42fc8768
patrickhuber <patrickhuber@users.noreply.github.com>
2020-01-24 14:52:17 -0500

    adds query

--- tasks/applications-without-autoscaler/task.sh
+++ tasks/applications-without-autoscaler/task.sh
@@ -3,60 +3,22 @@
 set -uex
 
 source source-repo/scripts/init.sh
-source source-repo/scripts/install-cf.sh
-source source-repo/scripts/install-yq.sh
-source source-repo/scripts/install-jq.sh
+source source-repo/scripts/install-sqlite.sh
 
-# login to cf api 
-cf api $CF_TARGET --skip-ssl-validation
-cf login -u $CF_USERNAME -p $CF_PASSWORD < /dev/null
+export INPUT_DIR=database
 
-# get autoscaler guid
-cf curl /v2/services > services.json 
-export AUTOSCALER_GUID = `cat services.json | jq '.resources[] | select(.entity.label == "app-autoscaler") | .metadata.guid' -r`
+cat > query.txt <<EOF
+SELECT * 
+FROM APPS a
+LEFT JOIN SERVICE_BINDINGS sb
+  on sb.APP_ID = a.ID
+INNER JOIN SERVICE_INSTANCES si
+  on si.ID = sb.SERVICE_INSTANCE_ID
+INNER JOIN SERVICES s
+  on  s.ID = si.SERVICE_ID
+  and s.NAME = 'app-autoscaler'
+WHERE sb.ID is null;
+EOF
 
-# get service plan for service (usually only one)
-cf curl /v2/services/$AUTOSCALER_GUID/service_plans > autoscaler_service_plans.json
-export AUTOSCALER_SERVICE_PLAN_GUID=`cat data/autoscaler_service_plans.json | jq .resources[0].metadata.guid -r`
-
-# get all service instances for the autoscaler plan
-cf curl /v2/service_plans/$AUTOSCALER_SERVICE_PLAN_GUID/service_instances > autoscaler_service_instances.json
-
-# get all service bindings
-cf curl /v2/service_bindings > service_bindings.json
-
-# get orgs
-cf curl /v2/orgs > orgs.json
-
-# write the org exclusion as json
-echo $EXCLUDE_ORGS | yq r --to-json > exclude_orgs.json
-
-# get list of org guids filtered by exclusion list
-# form a comma delimited list for the organization_guids query parameter
-export ORGANIZATION_GUIDS=`cat orgs.json \
-| jq --slurpfile filter exclude_orgs.json \
-'[.resources[] | select( .name as $name | $filter[] | index($name) | not )] | [.[].guid] | join(",")' \
--r`
-
-# use the list of org guids to filter the app list
-cf curl /v3/apps?organization_guids=$ORGANIZATION_GUIDS > apps.json
-
-# form a query :
-## SELECT * 
-## FROM APPS a
-## LEFT JOIN ServiceBindings sb
-##   on sb.AppId = a.AppId
-## INNER JOIN ServiceInstances si
-##   on si.ServiceInstanceId = sb.ServiceInstanceId
-## INNER JOIN ServicePlans sp
-##   on sp.ServicePlanId = si.ServicePlanId
-## INNER JOIN Services s
-##   on s.ServiceId = sp.ServiceId
-## WHERE a.OrgId not in
-## ( 
-##   SELECT OrgId 
-##   FROM Organizations
-##   WHERE Name not in ("system")
-## ) 
-## and s.ServiceName = 'app-autoscaler'
-## and sb.ServiceBindingId is null
+# run the query
+sqlite3 $INPUT_DIR/database.db < query.txt 
