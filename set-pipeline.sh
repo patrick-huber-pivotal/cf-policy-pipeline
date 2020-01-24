c56054c2694d5ecd7c66421bf1efdc4981cfdbbd
patrickhuber <patrickhuber@users.noreply.github.com>
2020-01-24 07:45:33 -0500

    adds vars

--- set-pipeline.sh
+++ set-pipeline.sh
@@ -5,4 +5,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 
 # set the pipeline
-fly -t dell set-pipeline -c $DIR/pipeline.yml -p cf-policies
+fly -t dell set-pipeline -c $DIR/pipeline.yml -p cf-policies -l $DIR/vars.yml
