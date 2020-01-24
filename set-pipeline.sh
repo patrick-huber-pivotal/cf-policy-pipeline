cb809d555be580caf34551a3fe81ed5183a914d1
patrickhuber <patrickhuber@users.noreply.github.com>
2020-01-24 07:40:08 -0500

    adds deploy script. updates task permissions

---
+++ set-pipeline.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# get the current script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+# set the pipeline
+fly -t dell set-pipeline -c $DIR/pipeline.yml -p cf-policies
