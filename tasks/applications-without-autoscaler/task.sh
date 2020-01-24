692c2c50006bc48859ed9dec0b89a5aed0cde7b9
patrickhuber <patrickhuber@users.noreply.github.com>
2020-01-24 08:04:41 -0500

    updates task to print apps json

--- tasks/applications-without-autoscaler/task.sh
+++ tasks/applications-without-autoscaler/task.sh
@@ -5,5 +5,9 @@ set -uex
 source source-repo/scripts/init.sh
 source source-repo/scripts/install-cf.sh
 source source-repo/scripts/install-yq.sh
+source source-repo/scripts/install-jq.sh
 
-echo $EXCLUDE_ORGS
+cf api $CF_TARGET --skip-ssl-validation
+cf login -u $CF_USERNAME -p $CF_PASSWORD < /dev/null
+cf curl /v2/apps > apps.json
+cat apps.json
