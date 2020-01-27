e4ffd6942062aaec54dd0828130da7b39370c458
patrickhuber <patrickhuber@users.noreply.github.com>
2020-01-27 10:48:06 -0500

    adds query task

--- tasks/query/task.sh
+++ tasks/query/task.sh
@@ -7,5 +7,7 @@ source source-repo/scripts/install-sqlite.sh
 
 export INPUT_DIR=database
 
+echo $QUERY > query.txt
+
 # run the query
-sqlite3 $INPUT_DIR/database.db < echo $QUERY
+sqlite3 $INPUT_DIR/database.db < query.txt 
