d521cd5bd23acbb88e848359ad2c5610e6e3ed0b
“patrick-huber-pivotal” <patrick-huber-pivotal@users.noreply.github.com>
2020-02-04 18:22:18 +0000

    writes to output directory

--- tasks/query/task.sh
+++ tasks/query/task.sh
@@ -7,5 +7,7 @@ export INPUT_DIR=database
 echo ".headers on" > query.txt
 echo "$QUERY" >> query.txt
 
-# run the query
-sqlite3 $INPUT_DIR/database.db < query.txt | column -t -n -s'|'
+# run the query save the output as a csv file
+sqlite3 $INPUT_DIR/database.db < query.txt > results/report.csv
+cat results/report.txt | column -t -n -s'|' > results/report.txt
+cat results/report.txt
