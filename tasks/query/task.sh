5e8642332892626443680bee911d865f8fe1fb54
“patrick-huber-pivotal” <patrick-huber-pivotal@users.noreply.github.com>
2020-02-04 18:23:52 +0000

    adds error for testing

--- tasks/query/task.sh
+++ tasks/query/task.sh
@@ -11,3 +11,4 @@ echo "$QUERY" >> query.txt
 sqlite3 $INPUT_DIR/database.db < query.txt > results/report.csv
 cat results/report.txt | column -t -n -s'|' > results/report.txt
 cat results/report.txt
+exit 1
