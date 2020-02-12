6ed02888c34a559fc0cfe01e5d6342fe45d0c61c
patrickhuber <patrickhuber@users.noreply.github.com>
2020-02-12 11:06:39 -0500

    adds alert and query for 3 month certificate expiration

---
+++ tasks/alert-rows-exist/task.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ue
+
+INPUT_DIR=results
+
+# The count threshold. 
+# If the count is greater than this number, alert by exiting with non zero
+THRESHOLD=0
+
+if [ "$SKIP_HEADER" == true ]; then
+    THRESHOLD=1
+fi
+
+FILE_PATH=$INPUT_DIR/$FILE
+COUNT=$(wc -l $FILE_PATH)
+
+if ["$COUNT" > "$THRESHOLD" ]; then
+    >&2 echo "count $COUNT has exceeded the threshold $THRESHOLD"
+    exit 1
+fi
+
+echo "count $COUNT is below threshold $THRESHOLD"
