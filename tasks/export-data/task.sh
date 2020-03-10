a49088748ccf5ed242a839076d2d31aeacef8e58
patrick-huber-pivotal <patrick-huber-pivotal@users.noreply.github.com>
2020-03-10 17:09:17 +0000

    updates export loop

--- tasks/export-data/task.sh
+++ tasks/export-data/task.sh
@@ -11,20 +11,21 @@ cf login -u $CF_USERNAME -p $CF_PASSWORD < /dev/null
 cf curl /v3/organizations > $OUTPUT_DIR/orgs.json
 cf curl /v3/spaces > $OUTPUT_DIR/spaces.json
 cf curl /v3/apps > $OUTPUT_DIR/apps.json
-count=0
-echo "[" > app-summaries.json
-for app in $(cat $OUTPUT_DIR/apps.json | jq '.resources[].guid' -r)
-do
-  if [[ "$count" -gt 0 ]]; then
-    echo "," >> app-summaries.json
-  fi
-  cf curl /v2/apps/$app/summary >> $OUTPUT_DIR/app-summaries.json
-  let "count++"
-done
-echo "]" >> app-summaries.json
 cf curl /v2/service_instances > $OUTPUT_DIR/service-instances.json
 cf curl /v2/service_bindings > $OUTPUT_DIR/service-bindings.json
 cf curl /v2/service_plans > $OUTPUT_DIR/service-plans.json
 cf curl /v2/services > $OUTPUT_DIR/services.json
 om curl --path /api/v0/deployed/certificates > $OUTPUT_DIR/certificates.json
 om curl --path /api/v0/certificate_authorities > $OUTPUT_DIR/certificate_authorities.json
+
+# export app summary data for each app
+count=0
+echo "[" > app-summaries.json
+while read -r app; do
+  if [[ "$count" -gt 0 ]]; then
+    echo "," >> app-summaries.json
+  fi
+  cf curl /v2/apps/$app/summary >> $OUTPUT_DIR/app-summaries.json
+  let "count++"
+done << $(cat $OUTPUT_DIR/apps.json | jq '.resources[].guid' -r)
+echo "]" >> app-summaries.json
